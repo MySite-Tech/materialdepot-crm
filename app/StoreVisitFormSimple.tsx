f15719a7e720cb02957cdcc41187e68f4a0f06d9
ayushsharma-09 <ayush.sharma@materialdepot.in>
2026-05-09 12:33:18 +0530

    hyderabad option added in store visit form

--- app/StoreVisitFormSimple.tsx
+++ app/StoreVisitFormSimple.tsx
@@ -30,6 +30,7 @@ const mockApi = {
       { id: 2, name: 'Whitefield', displayName: 'Whitefield' },
       { id: 3, name: 'Yelahanka', displayName: 'Yelahanka' },
       { id: 4, name: 'HQ', displayName: 'HQ' },
+      { id: 5, name: 'Hyderabad', displayName: 'Hyderabad' },
     ];
   },
 
